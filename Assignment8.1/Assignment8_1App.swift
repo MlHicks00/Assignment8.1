dc02f2fd7a3bd5a9f5989aea72a6a0760c85cfb9
Mhicks <mlhicks2@yahoo.com>
2022-07-30 09:48:55 -0400

    Initial Commit

---
+++ Assignment8.1/Assignment8_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment8_1App.swift
+//  Assignment8.1
+//
+//  Created by Mia Hicks on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment8_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment8.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment8.1
+//
+//  Created by Mia Hicks on 7/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
